da0d7f344b90a6e685745a504c9a889c5995ada9
Sid Mann <sjmann2@outlook.com>
2022-09-05 11:19:16 -0500

    Create count of contestants method and add to project show page

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-
 RSpec.describe Project, type: :model do
   describe "validations" do
     it {should validate_presence_of :name}
@@ -11,5 +10,22 @@ RSpec.describe Project, type: :model do
     it {should belong_to :challenge}
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
+  end
+
+  describe 'instance methods' do
+    before :each do
+      @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+      @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+      ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+      ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
+    end
+
+    describe 'count_of_contestants' do
+      it 'returns the count of contestants associated with a project' do
+        expect(@boardfit.count_of_contestants).to eq(2)
+      end
+    end
   end
 end
