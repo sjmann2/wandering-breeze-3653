fc3218def65d35c2113746882c4faaf2ca6dd5e8
Sid Mann <sjmann2@outlook.com>
2022-09-05 09:59:59 -0500

    Add project name and material to show page

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -13,8 +13,8 @@ RSpec.describe 'the project show page' do
     @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
     @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
 
-    @news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-    @boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+    @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
   end
 
   describe 'User story 1' do
@@ -23,7 +23,7 @@ RSpec.describe 'the project show page' do
         describe 'I also see the theme of the challenge that this project belongs to' do
           it 'displays project name and material' do
             visit "/projects/#{@news_chic.id}"
-
+            save_and_open_page
             expect(page).to have_content('News Chic')
             expect(page).to have_content('Newspaper')
           end
