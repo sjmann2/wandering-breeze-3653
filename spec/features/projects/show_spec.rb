5db246100fc44bbe3b3df7c3a071aeabe3a14e32
Sid Mann <sjmann2@outlook.com>
2022-09-05 10:01:39 -0500

    Test for challenge the project belongs to

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -23,12 +23,16 @@ RSpec.describe 'the project show page' do
         describe 'I also see the theme of the challenge that this project belongs to' do
           it 'displays project name and material' do
             visit "/projects/#{@news_chic.id}"
-            save_and_open_page
+            
             expect(page).to have_content('News Chic')
             expect(page).to have_content('Newspaper')
           end
 
-          it 'displays the theme of the challenge the project belongs to'
+          it 'displays the theme of the challenge the project belongs to' do
+            visit "/projects/#{@news_chic.id}"
+
+            expect(page).to have_content('Recycled Material')
+          end
         end
       end
     end
