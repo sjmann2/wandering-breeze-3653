84e994893b61c673a1ab58f4faa689ef555c063f
Sid Mann <sjmann2@outlook.com>
2022-09-05 09:51:00 -0500

    Feature test for user story 1

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+
+# As a visitor,
+# When I visit a project's show page ("/projects/:id"),
+# I see that project's name and material
+# And I also see the theme of the challenge that this project belongs to.
+# (e.g.    Litfit
+#     Material: Lamp Shade
+#   Challenge Theme: Apartment Furnishings)
+RSpec.describe 'the project show page' do
+  before :each do
+    @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    @news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    @boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+  end
+
+  describe 'User story 1' do
+    describe 'When I visit a project show page' do
+      describe 'I see that projects name and material' do
+        describe 'I also see the theme of the challenge that this project belongs to' do
+          it 'displays project name and material' do
+            visit "/projects/#{@news_chic.id}"
+
+            expect(page).to have_content('News Chic')
+            expect(page).to have_content('Newspaper')
+          end
+
+          it 'displays the theme of the challenge the project belongs to'
+        end
+      end
+    end
+  end
+end
